4c9ad7e600e26ae23177662db79d2b9de89b2ad9
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-21 05:55:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,26 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], # Top row
+  [3, 4, 5], # Middle row
+  [6, 7, 8], # Bottom row
+  [0, 3, 6], # Left column
+  [1, 4, 7], # Middle column
+  [2, 5, 8], # Right column
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  i = 0
+  while i < 9
+    if board[WIN_COMBINATIONS[i][0]] == "X" and board[WIN_COMBINATIONS[i][1]] == "X" and board[WIN_COMBINATIONS[i][2]] == "X"
+      return WIN_COMBINATIONS[i]
+    elsif board[WIN_COMBINATIONS[i][0]] == "O" and board[WIN_COMBINATIONS[i][1]] == "O" and board[WIN_COMBINATIONS[i][2]] == "O"
+      return WIN_COMBINATIONS[i]
+    end
+    i += 1
+  end
+return {}
+end
